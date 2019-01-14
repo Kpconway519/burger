efe70fb0f1479e2a1887f252f85e548b4464488d
Kevin519 <kpconway519@gmail.com>
2019-01-13 19:29:47 -0500

    push

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,10 +5,27 @@ const router = express.Router();
 //model--only one for this project
 const burger = require("../models/burger.js");
 
+
+
+
+
 //putting routes here
 router.get("/", function(req, res) {
-res.send("Homepage")
+    //remember to go back in here and add in the Handlebars options object after "index"
+res.render("index")
+  });
+
+router.get("/api/burgers", function(req, res) {
+    //res.json the database with all of the burgers in it.
 })
+
+//any other routes I need to add?
+
+//I need a put route to add new burgers
+
+//I need a thing which tells the server to swap burger from devoured:true to :false. 
+
+
 //////////////////// END OF ROUTES /////////////////////////
 
 
